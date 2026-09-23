13abe83bcb9e32ab22c1c0aa7b26902b14e5cbef
Rupesh Kumar <rupeshkumar.sde@gmail.com>
2026-09-23 12:59:08 +0530

    played with findById() method of CrudRepository

--- src/main/java/com/rupeshkumar/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/rupeshkumar/springdatajpa/SpringDataJpaApplication.java
@@ -1,6 +1,7 @@
 package com.rupeshkumar.springdatajpa;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,27 +20,35 @@ public class SpringDataJpaApplication {
 		PlayerRepository playerBean = context.getBean(PlayerRepository.class);
 //		System.out.println(bean.getClass().getName());
 		
-		Player p1 = new Player();
-		p1.setPlayerId(102);
-		p1.setPlayerName("V Shehwag");
-		p1.setPlayerAge(45);
-		p1.setLocation("Delhi");
-		
-		//playerBean.save(p1); // upsert method (insert + update)
-		
+		/*
+		 * Player p1 = new Player(); p1.setPlayerId(102); p1.setPlayerName("V Shehwag");
+		 * p1.setPlayerAge(45); p1.setLocation("Delhi");
+		 * 
+		 * playerBean.save(p1); // upsert method (insert + update)
+		 */		
 		// save is polymorphic method - it checks first the data is present or not 
 		//if not then insert the record and if present then update the record	
 		
 		UserRepository userBean = context.getBean(UserRepository.class);
 		
-		User u1 = new User(101, "Ramesh", "Male", 25, "India");
-		//userBean.save(u1);
+		/*
+		 * User u1 = new User(101, "Ramesh", "Male", 25, "India"); userBean.save(u1);
+		 */
 		
-		User u2 = new User(102, "Akash", "Male", 23, "India");
-		User u3 = new User(103, "Sumit", "Male", 29, "India");
-		User u4 = new User(104, "John", "Male", 32, "USA");
 		
-		userBean.saveAll(Arrays.asList(u2,u3,u4));
+		/*
+		 * User u2 = new User(102, "Akash", "Male", 23, "India"); User u3 = new
+		 * User(103, "Sumit", "Male", 29, "India"); User u4 = new User(104, "John",
+		 * "Male", 32, "USA");
+		 * 
+		 * userBean.saveAll(Arrays.asList(u2,u3,u4));
+		 */
+		 
+		
+		Optional<User> byId = userBean.findById(102);
+		if(byId.isPresent()) {
+			System.out.println(byId.get());
+		}
 	}
 
 }
